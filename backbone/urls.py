0a06ce26e64057afa460a4d552be2932da50f3e1
Marius Vigariu <marius.vigariu@gmail.com>
2026-06-07 11:33:03 +0300

    add django debug toolbar and static urls

--- backbone/urls.py
+++ backbone/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from importlib import import_module
 from django.contrib import admin
 from django.urls import path, include
@@ -11,8 +12,16 @@ for app in settings.BACKBONE_APPS:
     try:
         app_urls = import_module(f"{app}.urls")
 
-        urlpatterns += [
-            path("", include(app_urls))
-        ]
+        urlpatterns += [path("", include(app_urls))]
     except ModuleNotFoundError:
         continue
+
+
+if settings.DEBUG:
+    urlpatterns += (
+        [
+            path("__debug__/", include("debug_toolbar.urls", "djdt")),
+        ]
+        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )
